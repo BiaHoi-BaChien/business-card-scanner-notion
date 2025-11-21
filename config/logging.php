3a6b6f72b151a060d5bd1a093c9b9ababae2dd3c
Pacho <93363437+BiaHoi-BaChien@users.noreply.github.com>
2025-11-21 19:08:25 +0700

    Fix default logging level configuration

--- config/logging.php
+++ config/logging.php
@@ -17,7 +17,7 @@ return [
         'daily' => [
             'driver' => 'daily',
             'path' => storage_path('logs/laravel.log'),
-            'level' => env('LOG_LEVEL', Level::Debug->name),
+            'level' => env('LOG_LEVEL', 'debug'),
             'days' => 14,
         ],
 
@@ -27,7 +27,7 @@ return [
             'with' => [
                 'stream' => 'php://stderr',
             ],
-            'level' => env('LOG_LEVEL', Level::Debug->name),
+            'level' => env('LOG_LEVEL', 'debug'),
         ],
 
         'null' => [
