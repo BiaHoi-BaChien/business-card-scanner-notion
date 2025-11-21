7b5b349f74084db0d1a4880e6fb308aee1c84792
Pacho <93363437+BiaHoi-BaChien@users.noreply.github.com>
2025-11-21 19:23:12 +0700

    Add business_card_to_notion route and redirect

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,14 @@
 use App\Services\PropertyConfigService;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function (PropertyConfigService $propertyConfigService) {
+// ① /business_card_to_notion 用のルートを追加
+Route::get('/business_card_to_notion', function (PropertyConfigService $propertyConfigService) {
     return view('app', [
         'propertyConfig' => $propertyConfigService->load(base_path()),
     ]);
+});
+
+// ② 直接 /register_business_card/public/ などでアクセスしたとき用に / も残したいならリダイレクトでも可
+Route::get('/', function () {
+    return redirect('/business_card_to_notion');
 });
