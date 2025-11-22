0111b05a56839c4879194c648666db8168f27759
Pacho <93363437+BiaHoi-BaChien@users.noreply.github.com>
2025-11-22 17:28:44 +0700

    Adjust routes for subdirectory deployment

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,14 @@
 use App\Services\PropertyConfigService;
 use Illuminate\Support\Facades\Route;
 
-// ① /business_card_to_notion 用のルートを追加
-Route::get('/business_card_to_notion', function (PropertyConfigService $propertyConfigService) {
+// ① ドキュメントルートが register_business_card/public を指す場合は / を使う
+Route::get('/', function (PropertyConfigService $propertyConfigService) {
     return view('app', [
         'propertyConfig' => $propertyConfigService->load(base_path()),
     ]);
 });
 
-// ② 直接 /register_business_card/public/ などでアクセスしたとき用に / も残したいならリダイレクトでも可
-Route::get('/', function () {
-    return redirect('/business_card_to_notion');
+// ② 直接 /register_business_card/ 配下にアクセスした既存のリンク向けにリダイレクトを残す
+Route::get('/register_business_card', function () {
+    return redirect('/');
 });
